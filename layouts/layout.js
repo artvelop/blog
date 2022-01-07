94bbd7c74f76b7256bbbb13ca1b3f45c7a5707d9
ruso99 <hondongk@naver.com>
2022-01-08 02:06:35 +0900

    [ADD] 작성자 이미지 깃허브로 수정

--- layouts/layout.js
+++ layouts/layout.js
@@ -1,26 +1,32 @@
-import Image from 'next/image'
-import Container from '@/components/Container'
-import TagItem from '@/components/TagItem'
-import { NotionRenderer, Equation, Code, Collection, CollectionRow } from 'react-notion-x'
-import BLOG from '@/blog.config'
-import formatDate from '@/lib/formatDate'
-import { useLocale } from '@/lib/locale'
-import { useRouter } from 'next/router'
-import Comments from '@/components/Comments'
+import Image from 'next/image';
+import Container from '@/components/Container';
+import TagItem from '@/components/TagItem';
+import {
+  NotionRenderer,
+  Equation,
+  Code,
+  Collection,
+  CollectionRow,
+} from 'react-notion-x';
+import BLOG from '@/blog.config';
+import formatDate from '@/lib/formatDate';
+import { useLocale } from '@/lib/locale';
+import { useRouter } from 'next/router';
+import Comments from '@/components/Comments';
 
-const mapPageUrl = id => {
-  return 'https://www.notion.so/' + id.replace(/-/g, '')
-}
+const mapPageUrl = (id) => {
+  return 'https://www.notion.so/' + id.replace(/-/g, '');
+};
 
 const Layout = ({
   children,
   blockMap,
   frontMatter,
   emailHash,
-  fullWidth = false
+  fullWidth = false,
 }) => {
-  const locale = useLocale()
-  const router = useRouter()
+  const locale = useLocale();
+  const router = useRouter();
   return (
     <Container
       layout="blog"
@@ -28,8 +34,7 @@ const Layout = ({
       description={frontMatter.summary}
       // date={new Date(frontMatter.publishedAt).toISOString()}
       type="article"
-      fullWidth={fullWidth}
-    >
+      fullWidth={fullWidth}>
       <article>
         <h1 className="font-bold text-3xl text-black dark:text-white">
           {frontMatter.title}
@@ -38,13 +43,19 @@ const Layout = ({
           <nav className="flex mt-7 items-start text-gray-500 dark:text-gray-400">
             <div className="flex mb-4">
               <a href={BLOG.socialLink || '#'} className="flex">
-                <Image
-                  alt={BLOG.author}
-                  width={24}
-                  height={24}
-                  src={`https://gravatar.com/avatar/${emailHash}`}
-                  className="rounded-full"
-                />
+                <svg
+                  height="20"
+                  aria-hidden="true"
+                  viewBox="0 0 16 16"
+                  version="1.1"
+                  width="20"
+                  fill="#fff"
+                  data-view-component="true"
+                  class="octicon octicon-mark-github v-align-middle">
+                  <path
+                    fill-rule="evenodd"
+                    d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
+                </svg>
                 <p className="ml-2 md:block">{BLOG.author}</p>
               </a>
               <span className="block">&nbsp;/&nbsp;</span>
@@ -52,12 +63,12 @@ const Layout = ({
             <div className="mr-2 mb-4 md:ml-0">
               {formatDate(
                 frontMatter?.date?.start_date || frontMatter.createdTime,
-                BLOG.lang
+                BLOG.lang,
               )}
             </div>
             {frontMatter.tags && (
               <div className="flex flex-nowrap max-w-full overflow-x-auto article-tags">
-                {frontMatter.tags.map(tag => (
+                {frontMatter.tags.map((tag) => (
                   <TagItem key={tag} tag={tag} />
                 ))}
               </div>
@@ -73,7 +84,7 @@ const Layout = ({
                 equation: Equation,
                 code: Code,
                 collection: Collection,
-                collectionRow: CollectionRow
+                collectionRow: CollectionRow,
               }}
               mapPageUrl={mapPageUrl}
             />
@@ -84,23 +95,21 @@ const Layout = ({
         <a>
           <button
             onClick={() => router.push(BLOG.path || '/')}
-            className="mt-2 cursor-pointer hover:text-black dark:hover:text-gray-100"
-          >
+            className="mt-2 cursor-pointer hover:text-black dark:hover:text-gray-100">
             ← {locale.POST.BACK}
           </button>
         </a>
         <a>
           <button
             onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
-            className="mt-2 cursor-pointer hover:text-black dark:hover:text-gray-100"
-          >
+            className="mt-2 cursor-pointer hover:text-black dark:hover:text-gray-100">
             ↑ {locale.POST.TOP}
           </button>
         </a>
       </div>
       <Comments frontMatter={frontMatter} />
     </Container>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
